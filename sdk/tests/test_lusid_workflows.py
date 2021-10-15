1e7bea72893f55c39e2380614a9a96f3711d2c69
Mark Needham <mark.needham@finbourne.com>
2021-10-15 15:42:45 +0100

    add tests

---
+++ sdk/tests/test_lusid_workflows.py
@@ -0,0 +1,28 @@
+import unittest
+import json
+import logging
+import os
+
+import lusid_scheduler
+from lusid_scheduler import JobsApi
+from fbnsdkutilities import ApiClientFactory
+
+class LusidWorkflowsTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+
+        cls.logger = logging.getLogger()
+        cls.logger.setLevel(logging.INFO)
+
+        cls.api_factory = ApiClientFactory(lusid_scheduler, api_secrets_filename="secrets.json")
+        cls.api = cls.api_factory.build(lusid_scheduler.api.JobsApi)
+
+    def test_get_types(self):
+
+        response = self.api.list_jobs().values
+        self.assertGreaterEqual(len(response), 0, response)
+
+
+if __name__ == '__main__':
+    unittest.main()
